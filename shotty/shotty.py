33ebbb03da36e52dac5ebd26757be8a0e475ceee
Sean Haskell <sean.haskell@metoffice.gov.uk>
2018-06-11 11:51:55 +0100

    Added reboot command

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -165,6 +165,24 @@ def start_instances(project):
 	return		
 
 
+@instances.command('reboot')
+@click.option('--project',default=None, 
+	help='Only instances for project')
+def reboot_instances(project):
+	"Reboot EC2 instances"
+	instances = filter_instances(project)
+	
+	for i in instances:
+		print("Rebooting {0}...".format(i.id))
+		try:
+			i.reboot()
+		except botocore.exceptions.ClientError as e:
+			print(" Could not reboot {0}. ".format(i.id) + str(e))
+			continue
+			
+			
+	return	
+	
 	
 if __name__ == '__main__':
 	cli()
