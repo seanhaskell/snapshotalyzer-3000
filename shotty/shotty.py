87e294c7020cf3dfea7cd32a8a986650c6d34d35
Sean Haskell <sean.haskell@metoffice.gov.uk>
2018-06-11 09:49:41 +0100

    Don't start a snapshot if there is already one pending

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -18,6 +18,10 @@ def filter_instances(project):
 	
 	return instances
 
+def has_pending_snapshot(volume):
+	snapshots = list(volume.snapshots.all())
+	return snapshots and snapshots[0].state == 'pending'
+	
 @click.group()
 def cli():
 	"""Shotty manages snapshots"""
@@ -91,8 +95,12 @@ def create_snapshots(project):
 		i.stop()
 		i.wait_until_stopped()
 		for v in i.volumes.all():
-			print(" Creating snapshot of {0}".format(v.id))
-			v.create_snapshot(Description="Created by SnapshotAlyzer")
+		if has_pending_snapshot(v):
+			print("  Skipping {0}, snapshot already in progress".format(v.id))
+			continue
+			
+		print(" Creating snapshot of {0}".format(v.id))
+		v.create_snapshot(Description="Created by SnapshotAlyzer")
 			
 		print("Starting {0}...".format(i.id))
 		
